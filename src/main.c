3d3d42b734ec30897cec8e5a75ba08674d7bef2a
Diogo <diogocheng@gmail.com>
2017-08-05 11:42:00 -0300

    Todos os testes OK

--- src/main.c
+++ src/main.c
@@ -10,16 +10,55 @@
 int main() {
 
   char c = 0;
-  int cont = 0, word = 0;  
+  int cont = 0, word = 0, ehnum = 0, pontoDec = 0;    
 
   while (c != '\n') {
     scanf("%c", &c);
-    if ((word == 0) && ((97 <= c && c <= 122) || (65 <= c && c <= 90) || (48 <= c && c <= 57))){
-    	word = 1;    	
-    	cont ++;
+    if (word == 0){ // Possível começo de palavra
+      if (48 <= c && c <= 57){ // É um número
+        ehnum = 1;
+        word = 1;
+        cont ++;        
+      }
+      else if ((65 <= c && c <= 90) || (97 <= c && c <= 122)){ // É uma letra
+        word = 1;
+        cont ++;        
+      }
     }
-    else if ((word == 1) && !((97 <= c && c <= 122) || (65 <= c && c <= 90) || (48 <= c && c <= 57))){
-    	word = 0;    	
+    else { // Possível fim de palavra
+      if (ehnum == 1){ // Palavra de número
+        if ((65 <= c && c <= 90) || (97 <= c && c <= 122)){ // Começo de palavra com letra logo em seguida do fim de número
+          ehnum = 0;
+          pontoDec = 0;
+          cont++;
+        }
+        else if (!((97 <= c && c <= 122) || (65 <= c && c <= 90) || (48 <= c && c <= 57))){ // Fim de número
+          if (c == '.' || c == ','){
+            if (pontoDec == 0){
+              pontoDec = 1;
+            }
+            else {
+              pontoDec = 0;
+              ehnum = 0;
+              word = 0;
+            }
+          }
+          else {
+            ehnum = 0;
+            pontoDec = 0;
+            word = 0;
+          }
+        }
+      }
+      else { // Palavra de letra
+        if (48 <= c && c <= 57){ // Começo de palavra com número logo em seguida do fim de letra
+          ehnum = 1;
+          cont ++;
+        }
+        else if (!((97 <= c && c <= 122) || (65 <= c && c <= 90) || (48 <= c && c <= 57))){ // Fim de letra
+          word = 0;
+        }
+      }     
     }
   }
 
